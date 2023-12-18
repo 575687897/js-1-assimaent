bcf53acfd2a2745e7146d9b7fbe4f8532989cd86
575687897 <mw8749504@gmail.com>
2023-12-18 03:41:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,155 @@
+// Write a program that allow to user enter number then printit(((1)))
+// var nam1="";
+// document.getElementById("DEMO").innerHTML=nam1 =prompt('PLEAS INTER NAMBER');
+// ************************************************************************************
+
+
+// - Write a program that take number from user then print yes if that number can divide by 3
+// and 4 otherwise print no(((2)))
+
+// var nam1=Number(prompt("PLEAS INTER NAMBER"))
+// var namber=Number(nam1)
+
+// if(namber % 3 === 0 && namber % 4 === 0){
+//     console.log("yes")
+   
+//  }
+//  else {
+//     console.log("no")}
+//     ************************************************************************************
+//     3- Write a program that allows the user to insert 2 integers then print the max(((3)))
+//     var number1 = Number(prompt("Enter the first number: "));
+//     var number2 = Number(prompt("Enter the second number: "));
+
+
+//     var maxnamber=Math.max(number1,number2)
+//     console.log(`The maximum number is: ${maxnamber}`);
+//     ************************************************************************************
+
+//     4- Write a program that allows the user to insert an integer then print negative if it is
+// negative number otherwise print positive.(((4)))
+
+
+// var number1 = Number(prompt("Enter the first number: "));
+// var nam=Number(number1)
+// if(nam<0){
+//     console.log("negative")
+// }
+// else{
+//     console.log("positive")
+// }
+// ************************************************************************************
+
+
+
+// Write a program that take 3 integers from user then print the max element
+// and the min element.(((5)))
+
+// var nam1=Number(prompt("PLEAS INTER NAMBER"))
+// var nam2=Number(prompt("PLEAS INTER NAMBER"))
+// var nam3=Number(prompt("PLEAS INTER NAMBER"))
+// var nam=Math.max(nam1,nam2,nam3)
+// console.log(nam);
+// var nam2=Math.min(nam1,nam2,nam3)
+// console.log(nam2);
+// ************************************************************************************
+// Write a program that allows the user to insert integer number then
+// check If a number is oven or odd  (((6)))
+
+// var nam=Number(prompt("PLEAS INTER NAMBER"))
+// if(nam%2===0){
+//     console.log(" even")
+// }
+// else {
+//     console.log("odd")
+// }
+// ************************************************************************************
+
+// 8- Write a program that take character from user then if it is vowel chars (a,e,I,o,u)
+// then print vowel otherwise print consonant(((8)))  
+// var nam=prompt("PLEAS INTER NAMBER")
+
+// var nam1=["a","i","o","u","e"];
+
+// if (nam.includes(nam)){
+//     console.log("helll")
+// }else{
+//     console.log("nooooo")
+// }
+// ************************************************************************************
+// 9- Writeaprogramthat allowsusertoinsertintegerthenprintallnumbersbetween1to
+// that’s number
+// Example Input (((9)))
+
+
+
+// var  user = Number(prompt("Enter an integer: "));
+
+
+// for ( i = 0; i <= user; i++) {
+//   console.log(i); 
+// }
+
+// ************************************************************************************
+
+
+// var  user = Number(prompt("Enter an integer: "));
+
+
+// for(i=0;i<=12;i++){
+//     console.log(user*i)
+// }
+// ************************************************************************************
+
+// ************************************************************************************
+// Writeaprogramthatallowstousertoinsertnumberthenprintallevennumbers
+// between 1 to this number(((10)))
+
+
+// var  user = Number(prompt("Enter an integer: "));
+
+
+// for(i=1;i<=user;i++){
+//     console.log(i-"1")
+// }
+
+// ************************************************************************************
+// 2- Writeaprogramthattaketwointegersthenprintthepower(((11)))
+// var  user1 = Number(prompt("Enter an integer: "));
+// var  user2 = Number(prompt("Enter an integer: "));
+
+
+// var user3=Math.pow(user1,user2);
+// console.log(user3)
+
+// ************************************************************************************
+
+// 2- Write a program to enter marks of five subjects and calculate total, average and
+// percentage.(((11)))
+
+// var  user1 = Number(prompt("Enter an integer: "));
+// var  user2 = Number(prompt("Enter an integer: "));
+// var  user3 = Number(prompt("Enter an integer: "));
+// var  user4 = Number(prompt("Enter an integer: "));
+// var  user5 = Number(prompt("Enter an integer: "));
+
+
+// var CONTACT=(user1+user2+user3+user4+user5)/500*100;
+
+// console.log(CONTACT);
+
+// ************************************************************************************
+
+
+// ************************************************************************************
+
+// 3-Write a program to input month number and print number of days in that
+// month.(((12)))
+
+
+
+
+
+
+
+
